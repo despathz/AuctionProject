3a978d6d076926bebb954cff0c785907870b2087
Dimitris Anastasopoulos <Dimitris Anastasopoulos>
2016-07-18 18:35:47 +0300

    compose message

--- src/main/resources/static/messageControllers.js
+++ src/main/resources/static/messageControllers.js
@@ -10,9 +10,27 @@ myApp.controller('sentCtrl', ["$rootScope", '$scope', function($rootScope, $scop
     
 }]);
 
-myApp.controller('composeCtrl', ["$rootScope", '$scope', function($rootScope, $scope) {
-    $scope.compose = {textbody: "", sentUsername: ""};
+myApp.controller('composeCtrl', ["$rootScope", '$scope', '$http', function($rootScope, $scope, $http) {
+    $scope.compose = {textbody: "", username: "", title: ""};
+    
     $scope.eraseText = function() {
         $scope.compose.textbody = ""; 
     };
+    
+    $scope.sendMessage = function() {
+        $scope.errors = {bodyError: false, usernameError: false, titleError: false};
+        if ($scope.compose.username === "")
+            $scope.errors.usernameError = true;
+        if ($scope.compose.textbody === "")
+            $scope.errors.bodyError = true;
+         if ($scope.compose.title === "")
+            $scope.errors.titleError = true;
+        var res = $http.post('/ws/user/register/checkUsername', $scope.compose);
+        res.success(function(response) {
+            $scope.errors.usernameError = !response;
+            if (!$scope.errors.usernameError && !$scope.errors.bodyError && !$scope.errors.titleError) {
+                
+            }
+        });
+    }
 }]);
